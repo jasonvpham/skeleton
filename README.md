774b6e82f1661887fc4271faf0f6fe1c221f395f
Jason Pham <jvp@Phamily-2.local>
2019-04-25 16:57:43 +0100

    Added front end dashboard

---
+++ README.md
@@ -0,0 +1,8 @@
+# Skeleton
+
+A starting point for a Symfony system.
+
+<b>Backend:</b> Symfony 4.2.7 https://symfony.com <br> 
+<b>Frontend:</b> Admin Lte 3.0 https://adminlte.io/
+
+
---
+++ src/Controller/Dashboard/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller\Dashboard;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="default")
+     */
+    public function index()
+    {
+        return $this->render('dashboard/dashboard.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+}
